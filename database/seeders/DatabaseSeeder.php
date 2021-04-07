fc0af09a70fe5935833f54f44bd3c5ca6642008c
JuanPabloZapataA <juanp.zapataa@autonoma.edu.co>
2021-04-07 18:09:07 -0500

    Seeder para tiendas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersSeeder::class,
             WalkersSeeder::class,
-            StoreOwnersSeeder::class
+            StoreOwnersSeeder::class,
+            StoresSeeder::class
         ]);
     }
 }
