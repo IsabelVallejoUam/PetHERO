b010d93c474023007fef34e53e371075efd47ce0
JuanPabloZapataA <juanp.zapataa@autonoma.edu.co>
2021-04-27 23:33:50 -0500

    Se corrigió un buf menor

--- database/migrations/2021_04_19_225432_create_favorite_pets_table.php
+++ database/migrations/2021_04_19_225432_create_favorite_pets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFavoritePetOwnersTable extends Migration
+class CreateFavoritePetsTable extends Migration
 {
     /**
      * Run the migrations.
