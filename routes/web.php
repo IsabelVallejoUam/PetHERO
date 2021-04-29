118224ed9d11f759fea341326138789609b76ad5
IsabelVallejoUam <64813658+IsabelVallejoUam@users.noreply.github.com>
2021-04-29 15:10:38 -0500

    movi rutas para que se vea mas organizado

--- routes/web.php
+++ routes/web.php
@@ -23,36 +23,28 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('Inicio.welcome');});
-
 Route::view('/registerOptions', 'Inicio.register');
-
 Route::view('/lobby', 'Inicio.lobby');
-
-
-//Route::get('/walker/show/{document}', [WalkerController::class, 'show']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::resource('/walker', WalkerController::class);
+Route::get('/walker/profile/{walker}', [App\Http\Controllers\WalkerController::class, 'profile'])->name('walker.profile');
+Route::post('/walker/favorite/pet/{pet}', [App\Http\Controllers\WalkerController::class, 'addFavoritePet'])->name('walker.addFavoritePet');
 
 Route::resource('/storeOwner', StoreOwnerController::class);
 
 Route::resource('/petOwner', PetOwnerController::class);
+Route::get('/petOwner/profile/{petOwner}', [App\Http\Controllers\PetOwnerController::class, 'profile'])->name('petOwner.profile');
+Route::post('/petOwner/favorite/walker/{walker}', [App\Http\Controllers\PetOwnerController::class, 'addFavoriteWalker'])->name('petOwner.addFavoriteWalker');
+Route::post('/petOwner/favorite/store/{store}', [App\Http\Controllers\PetOwnerController::class, 'addFavoriteStore'])->name('petOwner.addFavoriteStore');
 
 Route::resource('/walkRequest',WalkController::class);
 
 Route::resource('/store',StoreController::class);
 
 Route::resource('/product',ProductController::class);
-
-Route::get('/walker/profile/{walker}', [App\Http\Controllers\WalkerController::class, 'profile'])->name('walker.profile');
-
-Route::get('/petOwner/profile/{petOwner}', [App\Http\Controllers\PetOwnerController::class, 'profile'])->name('petOwner.profile');
-
-Route::post('/petOwner/favorite/walker/{walker}', [App\Http\Controllers\PetOwnerController::class, 'addFavoriteWalker'])->name('petOwner.addFavoriteWalker');
-Route::post('/petOwner/favorite/store/{store}', [App\Http\Controllers\PetOwnerController::class, 'addFavoriteStore'])->name('petOwner.addFavoriteStore');
 
 Route::post('/useravatar', [App\Http\Controllers\UserController::class,'update_avatar']);
 Route::post('/productdata', [App\Http\Controllers\ProductController::class,'getData'])->name('product.getData');
