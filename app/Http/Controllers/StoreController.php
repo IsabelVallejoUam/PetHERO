ff74eb6f587486a4331974d72751d6c5df29f7bf
JuanPabloZapataA <juanp.zapataa@autonoma.edu.co>
2021-04-29 00:54:47 -0500

    Se añadió ver todas las tiendas

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -15,7 +15,9 @@ class StoreController extends Controller
      */
     public function index()
     {
-        //
+        // Cambiar esto de aquí
+        $stores = Store::all();
+        return view('store.indexAll',compact('stores'));
     }
 
     /**
