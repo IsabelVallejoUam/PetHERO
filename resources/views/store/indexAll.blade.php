4e85bc2a252687b85c19a69f178a68da8909e5ef
JuanPabloZapataA <juanp.zapataa@autonoma.edu.co>
2021-05-04 00:00:50 -0500

    Arreglo bug listas verticales

--- resources/views/store/indexAll.blade.php
+++ resources/views/store/indexAll.blade.php
@@ -2,21 +2,26 @@
 
 @section('content')
 
-<div class="card container"> <h1>Establecimientos</h1> 
+<div class="card container"> 
+    <h1>
+        Establecimientos
+    </h1> 
+    <div class="container">
     @foreach ($stores as $store)
-    <div class="card" style="width: 18rem; display:inline-block; margin:10px;">
-        <img class="card-img-top" src="/uploads/stores/{{$store->photo}}" alt="">
-        <div class="card-body">
-            <h4 class="card-title"><b>{{$store->store_name}}</b></h4>
-            <h5> <i>{{$store->slogan}}</i></h5>
-            <h6> {{$store->description}}</h6>
-            <p><b>Horario:</b> {{$store->schedule}}</p>
-            <p><b>Dirección:</b> {{$store->address}}</p>
-            <p><b>Teléfono:</b> {{$store->phone_number}}</p>
-            <p><b>Puntuación:</b> {{$store->score}}</p>
-            <a href="{{ route('store.showPublic', $store->id) }}" class=" btn btn-info"> Ver {{$store->store_name}}</a>
+        <div class="card" style = "width: 20rem; margin:10px; display:inline-block;">
+            <img class="card-img-top" src="/uploads/stores/{{$store->photo}}" alt="">
+            <div class="card-body">
+                <h4 class="card-title"><b>{{$store->store_name}}</b></h4>
+                <h5> <i>{{$store->slogan}}</i></h5>
+                <h6> {{$store->description}}</h6>
+                <p><b>Horario:</b> {{$store->schedule}}</p>
+                <p><b>Dirección:</b> {{$store->address}}</p>
+                <p><b>Teléfono:</b> {{$store->phone_number}}</p>
+                <p><b>Puntuación:</b> {{$store->score}}</p>
+                <a href="{{ route('store.showPublic', $store->id) }}" class=" btn btn-info"> Ver {{$store->store_name}}</a>
+            </div>
         </div>
+    @endforeach
     </div>
-@endforeach
 </div>
 @endsection
--- resources/views/walker/index.blade.php
+++ resources/views/walker/index.blade.php
@@ -8,12 +8,14 @@
         <div class="container">
         @foreach ( $walkers as $walker) 
             <div class="card col-md-6" style = "width: 20rem; margin:10px; display:inline-block;" >
+                <h4 class="card-title text-center"><b> {{$walker->name}}</b></h4>
                 <img class="card-img-top" src="/uploads/avatars/{{$walker->avatar}}">
                 <div class="card-body">
-                    <h5 class="card-title"><b> {{$walker->name}}</b></h5>
+                    <p class="card-text text-center"><i>"{{$walker->slogan}}"</i></p>       
                     <p class="card-subtitle mb-2 text-muted"><b>Puntuacion:{{$walker->score}}</b></p>
-                    <p class="card-text">Slogan:{{$walker->slogan}}</p>
-                    <a href="{{ route('walker.profile', $walker->user_id) }}" class="btn btn-secondary">Ver Perfil</a>
+                    <p class="text-center">
+                        <a href="{{ route('walker.profile', $walker->user_id) }}" class="btn btn-secondary">Ver Perfil</a>
+                    </p>  
                 </div>
             </div>  
         @endforeach 
