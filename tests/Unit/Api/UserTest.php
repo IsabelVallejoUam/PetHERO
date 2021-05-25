9c0d72fc9fd83516e492112e2cbb39955d1d0a59
NicolasHerreraS <nicolas.herreras@autonoma.edu.co>
2021-05-24 22:03:36 -0500

    Cambios al test de usuarios
    Se agregaron nuevos test a usuario

--- tests/Unit/Api/UserTest.php
+++ tests/Unit/Api/UserTest.php
@@ -58,11 +58,11 @@ class UserTest extends TestCase
         $response->assertStatus(422);
     }
 
-    public function testCrearUsuarioNombreIncorrecto()
+    public function testCrearUsuarioApellidoIncorrecto()
     {
         $response = $this->postJson('/api/v1/users', [
-            'name' => '', 
-            'lastname' => 'En Test',
+            'name' => 'Link Creado', 
+            'lastname' => '',
             'email' => 'linkuardo@yahoo.com',
             'document' => '102010201',
             'phone' => '310292929',
@@ -71,6 +71,57 @@ class UserTest extends TestCase
         $response->assertStatus(422);
     }
 
+    public function testCrearUsuarioCorreoIncorrecto()
+    {
+        $response = $this->postJson('/api/v1/users', [
+            'name' => 'Link Creado', 
+            'lastname' => 'En Test',
+            'email' => '112',
+            'document' => '102010201',
+            'phone' => '310292929',
+            'password' => "hola123"]);
+        $lastId = User::max('id');
+        $response->assertStatus(422);
+    }
+
+    public function testCrearUsuarioDocumentoIncorrecto()
+    {
+        $response = $this->postJson('/api/v1/users', [
+            'name' => 'Link Creado', 
+            'lastname' => 'En Test',
+            'email' => 'linkuardo@yahoo.com',
+            'document' => '12',
+            'phone' => '310292929',
+            'password' => "hola123"]);
+        $lastId = User::max('id');
+        $response->assertStatus(422);
+    }
+
+    public function testCrearUsuarioTelefonoIncorrecto()
+    {
+        $response = $this->postJson('/api/v1/users', [
+            'name' => 'Link Creado', 
+            'lastname' => 'En Test',
+            'email' => 'linkuardo@yahoo.com',
+            'document' => '102010201',
+            'phone' => '90900909000909090909090909',
+            'password' => "hola123"]);
+        $lastId = User::max('id');
+        $response->assertStatus(422);
+    }
+
+    // public function testCrearUsuarioNoContrasena()
+    // {
+    //     $response = $this->postJson('/api/v1/users', [
+    //         'name' => 'Link Creado', 
+    //         'lastname' => 'En Test',
+    //         'email' => 'linkuardo@yahoo.com',
+    //         'document' => '102010201',
+    //         'phone' => '310292929',
+    //         'password' => NULL]);
+    //     $lastId = User::max('id');
+    //     $response->assertStatus(422);
+    // }
 
     public function testBorrarUsuario(){
         $lastId = User::max('id');
