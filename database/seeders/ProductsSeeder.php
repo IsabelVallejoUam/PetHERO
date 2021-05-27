57e5a3ad709e20b6765f25d3e6c97db2b7bdddc0
JuanMora21 <74797654+JuanMora21@users.noreply.github.com>
2021-05-26 20:46:28 -0500

    Pequeños cambios en el seeder de Product

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -14,79 +14,85 @@ class ProductsSeeder extends Seeder
     public function run()
     {
         DB::table('products')->insert([
-            'store_id' => "3", 
+            'store_id' => "5", 
             'price' => "10000",
             'name' => "Alimento dog chow 500gr",
             'discount' => "0",
             'quantity' => "20",
+            'privacy' => "public",
             'description' => "Alimento premium para perros",
-            'photo' => "docg_chow.png"
+
         ]); 
 
         DB::table('products')->insert([
-            'store_id' => "3", 
+            'store_id' => "5", 
             'price' => "18000",
             'name' => "Alimento dog chow 1000gr",
             'discount' => "0",
             'quantity' => "20",
+            'privacy' => "public",
             'description' => "Alimento premium para perros",
-            'photo' => "docg_chow.png"
+
         ]); 
 
         DB::table('products')->insert([
-            'store_id' => "1", 
+            'store_id' => "6", 
             'price' => "5000",
             'name' => "Shampoo Canino",
             'discount' => "0",
             'quantity' => "10",
+            'privacy' => "public",
             'description' => "Shampoo premium para perros",
 
         ]); 
 
         DB::table('products')->insert([
-            'store_id' => "2", 
+            'store_id' => "7", 
             'price' => "10000",
             'name' => "Cama Negra",
             'discount' => "10",
             'quantity' => "20",
+            'privacy' => "public",
             'description' => "Cama acolchonada para animalitos medianos",
         ]); 
 
         DB::table('products')->insert([
-            'store_id' => "2", 
+            'store_id' => "7", 
             'price' => "8000",
             'name' => "Alimento dog chow 500gr",
             'discount' => "0",
             'quantity' => "20",
             'description' => "Alimento premium para perros seniors",
-            'photo' => "docg_chow.png"
+            'privacy' => "public",
         ]); 
         DB::table('products')->insert([
-            'store_id' => "3", 
+            'store_id' => "5", 
             'price' => "50000",
             'name' => "consulta medica",
             'type' => "servicio",
             'discount' => "0",
             'quantity' => "5",
+            'privacy' => "public",
             'description' => "Consulta general para mascotas",
         ]); 
         DB::table('products')->insert([
-            'store_id' => "4", 
+            'store_id' => "8", 
             'price' => "10000",
             'name' => "Alimento dog chow 500gr",
             'discount' => "0",
             'quantity' => "50",
+            'privacy' => "public",
             'description' => "Alimento premium para perros",
-            'photo' => "docg_chow.png"
+
         ]); 
         DB::table('products')->insert([
-            'store_id' => "5", 
+            'store_id' => "9", 
             'price' => "50000",
             'name' => "Alimento dog chow 250gr",
             'discount' => "0",
             'quantity' => "100",
+            'privacy' => "public",
             'description' => "Alimento premium para perros",
-            'photo' => "docg_chow.png"
         ]); 
     }
 }
