7f781bcc6774ba9a183a66e83af4c3528d91c70d
JuanMora21 <74797654+JuanMora21@users.noreply.github.com>
2021-05-31 07:18:05 -0500

    Pequeños arreglos graficos

--- resources/views/favoriteStore/index.blade.php
+++ resources/views/favoriteStore/index.blade.php
@@ -2,22 +2,24 @@
 
 @section('content')
 
-<div class="card container"> <h1>Establecimientos Favoritos</h1> 
-  
-    @foreach ($stores as $store)
-    <div class="card" style="width: 18rem; display:inline-block; margin:10px;">
-        <img class="card-img-top" src="/uploads/stores/{{$store->photo}}" alt="{{$store->description}}">
-        <div class="card-body">
-            <h4 class="card-title"><b>{{$store->store_name}}</b></h4>
-            <h5> <i>{{$store->slogan}}</i></h5>
-            <h6> {{$store->description}}</h6>
-            <p><b>Horario:</b> {{$store->schedule}}</p>
-            <p><b>Dirección:</b> {{$store->address}}</p>
-            <p><b>Teléfono:</b> {{$store->phone_number}}</p>
-            <p><b>Puntuación:</b> {{$store->score}}</p>
-            <a href="{{ route('store.showPublic', $store->id) }}" class=" btn btn-info"> Ver {{$store->store_name}}</a>
-        </div>
+<div class="card container"> 
+    <h1>Establecimientos Favoritos</h1> 
+    <div class="row-fluid ">
+        @foreach ($stores as $store)
+            <div class="card" style="width: 18rem; display:inline-block; margin:10px;">
+                <img class="card-img-top" src="/uploads/stores/{{$store->photo}}" alt="{{$store->description}}">
+                <div class="card-body">
+                    <h4 class="card-title"><b>{{$store->store_name}}</b></h4>
+                    <h5> <i>{{$store->slogan}}</i></h5>
+                    <h6> {{$store->description}}</h6>
+                    <p><b>Horario:</b> {{$store->schedule}}</p>
+                    <p><b>Dirección:</b> {{$store->address}}</p>
+                    <p><b>Teléfono:</b> {{$store->phone_number}}</p>
+                    <p><b>Puntuación:</b> {{$store->score}}</p>
+                    <a href="{{ route('store.showPublic', $store->id) }}" class=" btn btn-info"> Ver {{$store->store_name}}</a>
+                </div>
+            </div>
+        @endforeach
     </div>
-@endforeach
 </div>
 @endsection
